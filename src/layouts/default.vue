337b7718d6fa0434a93e363b28f792d9886876a1
ivgtr <gear.1112t.o@gmail.com>
2020-06-11 01:31:25 +0900

    🍏 Fuck Safali really buggy

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,9 +7,7 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({
-  components: {}
-})
+export default Vue.extend({})
 </script>
 
 <style lang="scss" scoped>
@@ -19,7 +17,10 @@ export default Vue.extend({
   position: relative;
   justify-items: center;
   width: 100%;
-  height: 100vh;
+  min-height: 100%;
+  min-height: -moz-available;
+  min-height: -webkit-fill-available;
+  min-height: fill-available;
   background: no-repeat center center/300% 300%
     linear-gradient(130deg, #1cc6b8, #3f3f94, #ec2757, #fcfc67, #eb6d71);
 }
