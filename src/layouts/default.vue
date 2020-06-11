6f33c548359eb6a982e3a6cac7dce9cb05c69abe
ivgtr <gear.1112t.o@gmail.com>
2020-06-11 11:23:51 +0900

    🔧 ちょっと調整

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -21,9 +21,11 @@ export default Vue.extend({})
   background: no-repeat center center/300% 300%
     linear-gradient(130deg, #1cc6b8, #3f3f94, #ec2757, #fcfc67, #eb6d71);
 }
-::-webkit-full-page-media,
-:future,
+/* stylelint-disable */
+_::-webkit-full-page-media,
+_:future,
 :root .root {
   min-height: -webkit-fill-available;
 }
+/* stylelint-enable */
 </style>
