ee865b71d053aeb20af2efbf21bd8e1be4a1ec7d
ivgtr <gear.1112t.o@gmail.com>
2020-06-11 16:55:10 +0900

    📈 metaタグを調整したよ

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,15 +5,30 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
-    htmlAttrs: { lang: 'ja' },
+    title: process.env.npm_package_name || 'irotoridori',
+    htmlAttrs: { lang: 'ja', prefix: 'og: http://ogp.me/ns#' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'ivgtr.me'
+      },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'irotoridori' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://ivgtr.me' },
+      { hid: 'og:title', property: 'og:title', content: 'irotoridori' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'ivgtr.me'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content:
+          'https://lh3.googleusercontent.com/gLHukE-ObJ2hM-YMSUghThADn47rKXltREoExmIxTr7LiK2Kr-jPMOTGVcXl1oPkJLbTBChBr55z16u1AcG6OSlZtkBenh3zURNuDFJpGVkBzSGbjui_rVSfNEhPZaGBCtw3p26ptQ=w600'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
