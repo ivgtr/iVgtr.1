7895b22e9b0829f06b1e87f8ba96e5a7469856dc
ivgtr <gear.1112t.o@gmail.com>
2020-11-17 17:25:51 +0900

    :lipstick: change: errorページを修正

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,8 +1,13 @@
 <template>
   <div
-    class="rounded flex flex-wrap items-center justify-center flex-col sm:flex-row mx-2 my-2 py-16 sm:py-8 px-4 sm:px-16"
+    class="rounded flex items-center justify-center mx-2 my-2 py-16 sm:py-8 px-4 sm:px-16"
   >
-    <p>404 not found</p>
+    <div class="wrapper text-center">
+      <p>Page Not Found</p>
+      <nuxt-link class="inline-block mt-8 text-blue-200 font-bold" to="/"
+        >&lt;&lt;&nbsp;家に帰ろう
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
